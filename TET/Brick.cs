355da32d7d8c2024cfb02d1f777d422e7a66cb06
YoungShper <ivantamaz@gmail.com>
2022-03-22 15:21:37 +0300

    Tetris

---
+++ TET/Brick.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TET
+{
+    public abstract class Brick
+    {
+        private Rectangle[] _shape; // форма фигуры
+        private int _posX = 5; // начальные позиции
+        private int _posY = 0; //
+        private bool _stop = false;
+        public Color brickColor;
+        public int removes = 0; // убрано линий
+        public int PosX
+        {
+            get { return _posX; }
+        }
+
+        public int PosY
+        {
+            get { return _posY; }
+        }
+        public Rectangle[] Shape
+        {
+            get { return _shape; }
+            set { _shape = value; }
+        }
+
+        public bool Stop
+        {
+            get { return _stop; }
+            set { _stop = value; }
+
+        }
+        public void Draw(Graphics g) // отрисовка фигуры
+        {
+            foreach (Rectangle r in Shape)
+            {
+                if (r.Y >= 0 && r.Y < 500) g.FillRectangle(new SolidBrush(brickColor), r);
+            }
+        }
+
+        public abstract void Constructor(int cell);
+
+        public void Deleter(Graphics g, int cell, KeyEventArgs e) // закрашивание предыдущих позиций
+        {
+            foreach (Rectangle r in Shape)
+            {
+                if ((r.Y + cell != 400) || (r.Y + cell == 400 && (e != null && (e.KeyCode == Keys.D || e.KeyCode == Keys.A)))) g.FillRectangle(new SolidBrush(Color.Black), new Rectangle(r.X, r.Y, cell, cell));
+            }
+        }
+
+        public void Move(int cell, List<Brick> bricks) // движение фигуры
+        {
+            foreach (Rectangle r in Shape) // Если следующая позиция = концу поля, или координатам уже стоящей фигуры, текущая фигура останавливается
+            {
+                Stop = r.Y + cell == 400 ? true : false; 
+
+                foreach(Brick m in bricks)
+                {
+                    foreach(Rectangle b in m.Shape)
+                    {
+                        if (r.Y + cell == b.Y && r.X == b.X)
+                        {
+                            Stop = true;
+                            break;
+                        }
+                        if (Stop == true) break;
+                    }
+                }
+                if (Stop == true) break;
+            }
+
+            if (Stop != true)
+            {
+                for (int i = 0; i < Shape.Length; i++) Shape[i].Y += cell;
+            }
+            else { bricks.Add(this); }
+        }
+
+        public void GameOver(List<Brick> bricks, Timer t)
+        {
+            foreach (Brick r in bricks)
+            {
+                foreach(Rectangle b in r.Shape)
+                {
+                    for (int i = 0; i < bricks.Count; i++)
+                    {
+                        if (b.Y < 0)
+                        {
+                            Stop = true;
+                            t.Enabled = false;
+                            MessageBox.Show("Game over");
+                            break;
+                        }
+                    }
+                    if (b.Y < 0) break;
+                }
+            } 
+        }
+
+        public void Rotate(int cellSize, List<Brick> bricks)
+        {
+            Point[] shapeCheck = new Point[4];
+            int count = 0;
+            bool access = true;
+
+            foreach (Rectangle p in Shape) // заполняем временный массив
+            {
+                shapeCheck[count] = new Point(p.X, p.Y);
+                count++;
+            }
+
+            Point tmp = new Point(shapeCheck[1].X, shapeCheck[1].Y);
+
+            for (int i = 0; i < shapeCheck.Length; i++) // проверяем на выход за пределы поля и пересечения с фигурами во время вращения
+            {
+                Point currentFigure = new Point(shapeCheck[i].X, shapeCheck[i].Y);
+                shapeCheck[i].X = tmp.X + tmp.Y - currentFigure.Y;
+                shapeCheck[i].Y = currentFigure.X + tmp.Y - tmp.X;
+
+                if(shapeCheck[i].Y + cellSize > 400 || (shapeCheck[i].X + cellSize > 200 || shapeCheck[i].X < 0)) access = false;
+                foreach (Brick b in bricks)
+                {
+                    foreach(Rectangle r in b.Shape)
+                    {
+                        if (((r.X + cellSize == shapeCheck[i].X || r.X - cellSize == shapeCheck[i].X) && r.Y == shapeCheck[i].Y) && access == true) { access = false; break; }
+                    }
+                }
+                if (access == false) break;
+            }
+
+            if(access == true)
+            {
+                for (int i = 0; i < shapeCheck.Length; i++)
+                {
+                    Shape[i].X = shapeCheck[i].X;
+                    Shape[i].Y = shapeCheck[i].Y;
+                }
+            }
+        }
+
+        public void LineDelete(List<Brick> bricks, int cell)
+        {
+            int lastY = 400;
+            int counterOfRemoves = 0;
+            var tmp = this.Shape.GroupBy(rec => rec.Y).Select(grp => grp.First()).ToArray(); // находим разные значения Y упавшей детали
+            foreach(Rectangle f in tmp) // проверяем всю область рисования
+            {
+                List<int> indexOfBrick = new List<int>(); // хранение индексов объектов в которых удаляются квадраты
+                int counter = 0;
+                
+                foreach (Brick b in bricks)
+                {
+                    for(int j = 0; j < 4; j++)
+                    {
+                        if (b.Shape[j].Y == f.Y) { indexOfBrick.Add(bricks.IndexOf(b)); counter++; } // проверяем, есть ли 10 квадратов на одной линии
+                        if (counter == 10) break;                                                  // если есть - прерываем цикл 
+                    }
+
+                    if(counter == 10) 
+                    {
+                        foreach(int i in indexOfBrick) // удаляем квадраты по полученым индексам объектов(перемещаем за пределы разрешения, чтобы не мешали)
+                        {
+                            for(int j = 0; j < 4; j++)
+                            {
+                                if (bricks[i].Shape[j].Y == f.Y) { bricks[i].Shape[j] = new Rectangle(0, 500, 0, 0); counterOfRemoves++; lastY = lastY > f.Y ? f.Y : lastY; };
+                            }
+                        }
+                    }
+                }
+            }
+
+            counterOfRemoves /= 10;
+            removes += counterOfRemoves;
+            foreach (Brick m in bricks) // смещаем все элементы на количество удаленных строк
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    if(m.Shape[k].Y < 500) m.Shape[k].Y = m.Shape[k].Y < lastY ? m.Shape[k].Y + counterOfRemoves * cell : m.Shape[k].Y;
+                }
+            }
+        }
+    }
+
+    public class Line : Brick // палка
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4]; 
+            for(int i = 0; i < Shape.Length; i++)
+            {
+                Shape[i].X = PosX * cell;
+                Shape[i].Y = (PosY - i) * cell;
+                Shape[i].Width = cell;
+                Shape[i].Height = cell;
+            }
+        }
+    }
+
+    public class Square : Brick
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4]; 
+            for (int i = 0; i < Shape.Length; i++)
+            {
+                if(i < 2)
+                {
+                    Shape[i].X = PosX * cell;
+                    Shape[i].Y = (PosY - i) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+                else
+                {
+                    Shape[i].X = (PosX - 1) * cell;
+                    Shape[i].Y = (PosY - i + 2) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+            }
+        }
+    }
+
+    public class Zet : Brick
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4]; 
+            for (int i = 0; i < Shape.Length; i++)
+            {
+                if (i < 2)
+                {
+                    Shape[i].X = PosX * cell;
+                    Shape[i].Y = (PosY - 1 + i) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+                else
+                {
+                    Shape[i].X = (PosX - 1) * cell;
+                    Shape[i].Y = (PosY - i + 3) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+            }
+        }
+    }
+
+    public class Es : Brick
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4]; 
+            for (int i = 0; i < Shape.Length; i++)
+            {
+                if (i < 2)
+                {
+                    Shape[i].X = PosX * cell;
+                    Shape[i].Y = (PosY - i) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+                else
+                {
+                    Shape[i].X = (PosX - 1) * cell;
+                    Shape[i].Y = (PosY - i + 1) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+            }
+        }
+    }
+
+    public class El : Brick 
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4]; 
+            for (int i = 0; i < Shape.Length; i++)
+            {
+                if(i == 3)
+                {
+                    Shape[i].X = PosX * cell;
+                    Shape[i].Y = (PosY - i + 4) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+                else
+                {
+                    Shape[i].X = (PosX - 1) * cell;
+                    Shape[i].Y = (PosY - i + 1) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+            }
+
+        }
+    }
+
+    public class Jei : Brick
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4];
+            for (int i = 0; i < Shape.Length; i++)
+            {
+                if (i == 3)
+                {
+                    Shape[i].X = PosX * cell;
+                    Shape[i].Y = (PosY - i + 4) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+                else
+                {
+                    Shape[i].X = (PosX + 1) * cell;
+                    Shape[i].Y = (PosY - i + 1) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+            }
+
+        }
+    }
+
+    public class Tet : Brick
+    {
+        public override void Constructor(int cell)
+        {
+            Shape = new Rectangle[4];
+            for (int i = 0; i < Shape.Length; i++)
+            {
+                if(i < 3)
+                {
+                    Shape[i].X = PosX * cell;
+                    Shape[i].Y = (PosY - i) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+                else
+                {
+                    Shape[i].X = (PosX - 1) * cell;
+                    Shape[i].Y = (PosY - i + 2) * cell;
+                    Shape[i].Width = cell;
+                    Shape[i].Height = cell;
+                }
+            }
+        }
+    }
+}
---
+++ TET/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TET
+{
+    /*Дописать присваивание нулевым фигурам отрицательного значения и не двигать их*/
+    public partial class Form1 : Form
+    {
+        public static int cellSize = 20;
+        Graphics g, g2;
+        Brick figure;
+        Brick nextFigure;
+        public List<Brick> bricks = new List<Brick>();
+        Random random = new Random();
+        Random colorRandom = new Random();
+        KeyEventArgs keyEvent;
+        int nextBrick = 0;
+        int nextBrickColor;
+        int currentScore = 0;
+        int level = 0;
+        int lines = 0;
+        public Brick selectBrick(int next) // выбор фигуры
+        {
+            switch (next)
+            {
+                
+                case 0:
+                    {
+                        Line tmp = new Line();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                case 1:
+                    {
+                        Square tmp = new Square();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                case 2:
+                    {
+                        Zet tmp = new Zet();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                case 3:
+                    {
+                        Es tmp = new Es();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                case 4:
+                    {
+                        El tmp = new El();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                case 5:
+                    {
+                        Jei tmp = new Jei();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                case 6:
+                    {
+                        Tet tmp = new Tet();
+                        tmp.Constructor(cellSize);
+                        return tmp;
+                    }
+                default: return null;
+            }
+        }
+
+        public void ReDraw()
+        {
+            g.Clear(Color.Black);
+            foreach (Brick b in bricks)
+            {
+                b.Draw(g);
+            }
+        }
+
+        public void DrawNextFigure(Graphics gr, Brick b)
+        {
+            gr.Clear(Color.Black);
+            b.Draw(gr);
+        }
+
+        public Color PickColor(int a)
+        {
+            switch (a)
+            {
+                case 0: return Color.FromArgb(235, 102, 186); 
+                case 1: return Color.FromArgb(253, 188, 85); 
+                case 2: return Color.FromArgb(215, 246, 107);
+                case 3: return Color.FromArgb(92, 155, 253);
+                default: return Color.Black;
+            }
+        }
+
+        public void animation(Brick a, Graphics g, int cellSize, List<Brick> bricks)
+        {
+            a.Deleter(g, cellSize, keyEvent);
+            a.Move(cellSize, bricks);
+            a.Draw(g);
+            a.GameOver(bricks, timer1);
+        }
+
+        public Form1()
+        {
+            MessageBox.Show("A D - движение\n P - пауза\n S - ускорение\n Пробел - вращение фигуры");
+            nextBrick = random.Next(0, 7); // рандом для первой фигуры
+            nextBrickColor = colorRandom.Next(0, 4);
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            g2 = pictureBox2.CreateGraphics();
+            label1.Text = "Score: 0";
+            label2.Text = "Level: 0";
+            label3.Text = "Lines: 0";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            this.Focus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (figure == null) 
+            { 
+                figure = selectBrick(nextBrick); 
+                figure.brickColor = PickColor(nextBrickColor); 
+                nextBrick = random.Next(0, 7); 
+                nextBrickColor = colorRandom.Next(0, 4);
+                nextFigure = selectBrick(nextBrick); 
+                nextFigure.brickColor = PickColor(nextBrickColor);
+                for(int i = 0; i < 4; i++) // двигаем фигуру в окне
+                {
+                    nextFigure.Shape[i].X -= 3 * cellSize;
+                    nextFigure.Shape[i].Y += 3 * cellSize;
+                }
+                DrawNextFigure(g2, nextFigure);
+            } // если фигура null, создаем новый объект, рандом для следующей фигуры
+
+            if (figure.Stop == false) animation(figure, g, cellSize, bricks); // проигрываем анимацию, пока флаг остановки не активен
+            else 
+            {
+                figure.LineDelete(bricks, cellSize); // если фигура остановлена проверяем, можно ли удалить линию по ее координатам
+
+                switch (figure.removes) // счетчик очков
+                {
+                    case 0: break;
+                    case 1: currentScore += 10; break;
+                    case 2: currentScore += 30; break;
+                    case 3: currentScore += 60; break;
+                    case 4: currentScore += 100; break;
+                    default: currentScore += 100; break;
+                }
+                label1.Text = "Score: " + currentScore; // вывод текущих очков 
+                if (currentScore >= 200 && currentScore / 200 != level && timer1.Interval > 100) { timer1.Interval -= 30; level++; }
+                lines += figure.removes; // прибавляем убраные линии
+                label2.Text = "Level: " + level;
+                label3.Text = "Lines: " + lines;
+                var tmpBricks = bricks.Where(brick => brick.Shape[0].Height == 0 && brick.Shape[1].Height == 0 && brick.Shape[2].Height == 0 && brick.Shape[3].Height == 0).ToList();
+                // если есть фигуры со всеми пустыми прямоугольниками, удаляем их
+                foreach (Brick m in tmpBricks) bricks.Remove(m);
+                ReDraw(); // перерисовка
+                figure = null; // удаление фигуры для создания новой
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            keyEvent = e;
+
+            if(figure != null)
+            {
+                figure.Deleter(g, cellSize, keyEvent);
+                switch (e.KeyCode)
+                {
+                    case Keys.D:
+                        {
+                            bool plus = true; // флаг для смещения фигуры
+
+                            for (int i = 0; i < figure.Shape.Length; i++) // проверка на выход за пределы разрешения
+                            {
+                                if (figure.Shape[i].X + cellSize >= 200) { plus = false; break; }
+                            }
+
+                            if (plus == true)
+                            {
+                                for (int i = 0; i < figure.Shape.Length; i++) // проверка на пересечения с другими деталями
+                                {
+                                    foreach (Brick r in bricks)
+                                    {
+                                        foreach(Rectangle b in r.Shape)
+                                        {
+                                            if (figure.Shape[i].X + cellSize != b.X || figure.Shape[i].Y != b.Y) plus = true;
+                                            else { plus = false; break; }
+                                        }
+                                        if (plus == false) break;
+                                    }
+                                    if (plus == false) break;
+                                }
+                            }
+                            for (int i = 0; i < figure.Shape.Length; i++) figure.Shape[i].X += plus == true ? cellSize : 0; // смещение
+                            break;
+                        }
+
+                    case Keys.A:
+                        {
+                            bool minus = true;
+
+                            for (int i = 0; i < figure.Shape.Length; i++) // проверка на выход за пределы разрешения
+                            {
+                                if (figure.Shape[i].X + figure.Shape[i].Width - cellSize <= 0) { minus = false; break; }
+                            }
+
+                            if (minus == true)
+                            {
+                                for (int i = 0; i < figure.Shape.Length; i++)// проверка на пересечения с другими деталями
+                                {
+                                    foreach (Brick r in bricks)
+                                    {
+                                        foreach(Rectangle b in r.Shape)
+                                        {
+                                            if (figure.Shape[i].X - cellSize != b.X || figure.Shape[i].Y != b.Y) minus = true;
+                                            else { minus = false; break; }
+                                        }
+                                        if (minus == false) break;
+                                    }
+                                    if (minus == false) break;
+                                }
+                            }
+                            for (int i = 0; i < figure.Shape.Length; i++) figure.Shape[i].X -= minus == true ? cellSize : 0; // смещение
+                            break;
+                        }
+
+                    case Keys.Space: if (figure.GetType().Name != "Square") figure.Rotate(cellSize, bricks); break; // вращение фигур кроме квадрата
+                    case Keys.P: if (timer1.Enabled == true) timer1.Enabled = false; else timer1.Enabled = true; break; // Пауза
+                    case Keys.S: if(figure.Stop != true) animation(figure, g, cellSize, bricks); break; // ускорение
+                }
+                figure.Draw(g);
+            }
+        }
+    }
+}
